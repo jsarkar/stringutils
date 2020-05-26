4a721ed21235a535abc2e8cbf64cf0473d8a264b
Joy Sarkar <joy623227@gmail.com>
2020-05-26 19:49:26 -0400

    debug

--- stringutils.go
+++ stringutils.go
@@ -5,14 +5,20 @@ import (
 	"strings"
 )
 
+// Upper case string
+// TODO: Create some test case
 func Upper(s string) string {
 	return strings.ToUpper(s)
 }
 
+// Lower case string
+// TODO: Create some test case
 func Lower(s string) string {
 	return strings.ToLower(s)
 }
 
+// Count number of Word
+// TODO: Move the print statement out from the function
 func WordCount(s string) {
 	lines := strings.Split(s, "\n")
 	for i, l := range lines {
@@ -20,4 +26,15 @@ func WordCount(s string) {
 		fmt.Printf("%d. %s (WC: %d)\n", i, l, wc)
 	}
 
+}
+
+// character count
+func CharCount(s string) map[string]int {
+	m := make(map[string]int, 0)
+
+	for _, char := range s {
+		m[string(char)] = m[string(char)] + 1
+
+	}
+	return m
 }
